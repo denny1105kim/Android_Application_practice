15f00c44c8e7e6f9c799970953301d63920a3988
jaehyeonkim <pon98142@coasia.com>
2025-02-28 11:58:06 +0900

    [feat] counterApp: Add features for count/reset number
    This patch adds features for count/reset number.
    
    Signed-off-by: jaehyeonkim <pon98142@coasia.com>

--- 1_countNumber/app/src/main/java/com/practice/counterapp/MainActivity.kt
+++ 1_countNumber/app/src/main/java/com/practice/counterapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practice.counterapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,11 +15,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.practice.counterapp.ui.theme.CounterAppTheme
+import java.util.logging.Logger
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        var number = 0
+        resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = String.format(number.toString())
+        }
+        plusButton.setOnClickListener{
+            number +=1
+            numberTextView.text = String.format(number.toString())
+            Log.d("plusClick", "플러스 버튼이 클릭됐습니다. 숫자는 : ${number}")
+        }
+
+
     }
     }
