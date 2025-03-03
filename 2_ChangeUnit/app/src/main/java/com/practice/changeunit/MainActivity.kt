6cffa17c6bc545b5b681f4fed3c4b5b8998a4c31
jaehyeonkim <denny1105kim@gmail.com>
2025-03-03 23:17:23 +0900

    [feat] ChangeUnit : Complete app this patch completes ChangeUnit app.
    Signed-off-by: jaehyeonkim <denny1105kim@gmail.com>

--- 2_ChangeUnit/app/src/main/java/com/practice/changeunit/MainActivity.kt
+++ 2_ChangeUnit/app/src/main/java/com/practice/changeunit/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.practice.changeunit
 
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +13,44 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.practice.changeunit.databinding.ActivityMainBinding
 import com.practice.changeunit.ui.theme.ChangeUnitTheme
+import androidx.core.widget.addTextChangedListener
+import java.text.NumberFormat
+
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val outputText = binding.outputText
+        val inputButton = binding.inputButton
+        val inputEditText = binding.inputEditText
+        val swapImageButton = binding.inputButton
+
+
+        var inputNumber : Int = 0
+        var cmToMeter = true
+
+        swapImageButton.setOnClickListener {
+            cmToMeter = cmToMeter.not()
+            if(cmToMeter) {
+                inputEditText.text = "cm"
+            }
+        }
+
+        inputEditText.addTextChangedListener { text ->
+                inputNumber = if(text.isNullOrEmpty()){
+                    0 }
+                    else text.toString().toInt()
+            Log.d("inputNumber", inputNumber.toString())
+            var outputNumber = inputNumber.times(0.01)
+            outputText.text = outputNumber.toString()
+        }
+
     }
 }
